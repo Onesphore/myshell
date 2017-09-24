ed8ccbd6997c049d31d46682c5e0376fc64f071f
Onesphore <onesphore5@gmail.com>
2017-09-24 15:10:38 -0400

    Minor modification

--- myshell.c
+++ myshell.c
@@ -1,15 +1,3 @@
-/* See Chapter 5 of Advanced UNIX Programming:  http://www.basepath.com/aup/
- *   for further related examples of systems programming.  (That home page
- *   has pointers to download this chapter free.
- *
- * Copyright (c) Gene Cooperman, 2006; May be freely copied as long as this
- *   copyright notice remains.  There is no warranty.
- */
-
-/* To know which "includes" to ask for, do 'man' on each system call used.
- * For example, "man fork" (or "man 2 fork" or man -s 2 fork") requires:
- *   <sys/types.h> and <unistd.h>
- */
 #include <stdio.h>
 #include <sys/types.h>
 #include <unistd.h>
